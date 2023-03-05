0850aefc46483be35f949f70f4c280bae29ddd25
MaksTimoshenko <124436224+MakcTiMoshenko@users.noreply.github.com>
2023-03-05 10:57:03 +0300

    Задача 52. с функциями
    Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
    
    Например, задан массив:
    1 4 7 2
    5 9 2 3
    8 4 2 4
    Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.

---
+++ Exemple3/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в
+// каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int [m, n];
+    Random rand = new Random();
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            matrix[i,j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+double Sred(int[,] matr)
+{
+    double sum = 0;
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum += matr[i, j];
+        }
+    Console.Write($"{ sum / matr.GetLength(0)} ");
+}
+    return sum;
+}
+int m = ReadInt("Введите количетсво строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+System.Console.WriteLine("Среднее арифметическое элементов по столбцам = ");
+Sred(myMatrix);
